878fda2fb911e7f6a68fee498d88ce244b9d5aa9
mouncef909 <108595094+mouncef909@users.noreply.github.com>
2022-07-24 11:57:35 +0100

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,82 @@
+var s=document.getElementById('service')
+var c=document.getElementById('contact')
+const path = document.querySelector('#wave');
+const animation = document.querySelector('#moveTheWave');
+const m = 0.512286623256592433;
+
+function buildWave(w, h) {
+  
+  const a = h / 4;
+  const y = h / 2;
+  
+  const pathData = [
+    'M', w * 0, y + a / 2, 
+    'c', 
+      a * m, 0,
+      -(1 - a) * m, -a, 
+      a, -a,
+    's', 
+      -(1 - a) * m, a,
+      a, a,
+    's', 
+      -(1 - a) * m, -a,
+      a, -a,
+    's', 
+      -(1 - a) * m, a,
+      a, a,
+    's', 
+      -(1 - a) * m, -a,
+      a, -a,
+    
+    's', 
+      -(1 - a) * m, a,
+      a, a,
+    's', 
+      -(1 - a) * m, -a,
+      a, -a,
+    's', 
+      -(1 - a) * m, a,
+      a, a,
+    's', 
+      -(1 - a) * m, -a,
+      a, -a,
+    's', 
+      -(1 - a) * m, a,
+      a, a,
+    's', 
+      -(1 - a) * m, -a,
+      a, -a,
+    's', 
+      -(1 - a) * m, a,
+      a, a,
+    's', 
+      -(1 - a) * m, -a,
+      a, -a,
+    's', 
+      -(1 - a) * m, a,
+      a, a,
+    's', 
+      -(1 - a) * m, -a,
+      a, -a
+  ].join(' ');
+  
+  path.setAttribute('d', pathData);
+}
+
+buildWave(90, 60);
+
+c.style.display="none";
+s.style.display="none";
+function service(){
+	s.style.display="inline";
+	c.style.display="none";
+}
+function contact(){
+	c.style.display="inline";
+	s.style.display="none";
+} 
+function home(){
+	c.style.display="none";
+    s.style.display="none";
+}                                                                                                                                                                                                                                                            
+
